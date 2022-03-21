7f915dba2556974ff4afc2bfb63479dfb48d9932
Ayesha Ilyas <annie16950@gmail.com>
2022-03-21 14:10:59 -0400

    Initial Commit

---
+++ CalorieCalculator/CalorieCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalorieCalculatorApp.swift
+//  CalorieCalculator
+//
+//  Created by Ayesha Ilyas on 3/21/22.
+//
+
+import SwiftUI
+
+@main
+struct CalorieCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
